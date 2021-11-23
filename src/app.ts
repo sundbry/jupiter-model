e5a9b4afae2baf4258b2f5499057269eaac068b9
Ryan Sundberg <ryan@arctype.co>
2021-11-22 23:56:01 -0800

    Planet lighting

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { Geometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, SphereGeometry, WebGLRenderer } from 'three';
+import { Geometry, Material, Mesh, MeshLambertMaterial, PerspectiveCamera, Scene, SphereGeometry, SpotLight, SpotLightHelper, Vector3, WebGLRenderer } from 'three';
 
 const scene = new Scene();
 
@@ -16,20 +16,22 @@ class JupiterModel {
     this.renderer_ = new WebGLRenderer({canvas});
     this.camera_ = this.createCamera();
     this.planet_ = this.createPlanet();
+    this.scene_.add(this.planet_);
+    this.scene_.add(this.createSolarLight());
     this.animationFunction_ = (t) => this.render(t);
   }
 
   render(time: number): void {
-    time *= 0.001;  // convert time to seconds
-    this.planet_.rotation.x = time;
-    this.planet_.rotation.y = time;
+    time *= 0.001;  // convert 1time to seconds
+    this.planet_.rotation.z = -0.4;
+    this.planet_.rotation.x = 0.4;
+    this.planet_.rotation.y = time / 4;
     this.renderer_.render(this.scene_, this.camera_);
     requestAnimationFrame(this.animationFunction_);
   }
 
   initialize(): void {
     console.log("Initializing JupiterModel");
-    this.scene_.add(this.planet_);
     requestAnimationFrame(this.animationFunction_);
   }
 
@@ -38,11 +40,15 @@ class JupiterModel {
   }
 
   createPlanetGeometry(): Geometry {
-    return new SphereGeometry(/*radius=*/1, /*widthSegments=*/16, /*heightSegments=*/16);
+    return new SphereGeometry(/*radius=*/1, /*widthSegments=*/32, /*heightSegments=*/32);
   }
 
-  createPlanetMaterial(): MeshBasicMaterial {
-    return new MeshBasicMaterial({color: 0xd4ca48});
+  createPlanetMaterial(): Material {
+    return new MeshLambertMaterial({
+      color: 0xd4ca48,
+      emissive: 0x0000ff,
+      emissiveIntensity: 0.3,
+    });
   }
 
   createCamera(): PerspectiveCamera {
@@ -53,6 +59,14 @@ class JupiterModel {
     const camera = new PerspectiveCamera(fov, aspect, near, far);
     camera.position.z = 2;
     return camera;
+  }
+
+  createSolarLight(): SpotLight {
+    var spotlight = new SpotLight(0xffffff);
+    spotlight.position.set(-2, 3, 5);
+    var spotLightHelper = new SpotLightHelper(spotlight);
+    spotlight.add(spotLightHelper);
+    return spotlight;
   }
 
 }
